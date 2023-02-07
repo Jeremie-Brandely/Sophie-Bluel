e49cc1ade9af56cf50f13e299e5c9818ca0b557d
Jeremie Brandely <jeremie.brandely@gmail.com>
2023-02-07 12:44:36 +0100

    filtres terminés

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,36 +1,100 @@
-
-// Récupération des Works depuis l'api GET
-
+// RECUPERATION DES PROJETS ET AFFICHAGE //
 fetch("http://localhost:5678/api/works")
-.then(reponse => reponse.json())
-.then(works => {
-    for (let i = 0; i < works.length; i++) {
+    .then(reponse => reponse.json())
+    .then((works) => {
 
-        const article = works[i];
+        console.log(works);
+
+        function generateProjets(works) {
+            for (let i = 0; i < works.length; i++) {
+    
+                const article = works[i];
+                
+                const sectionGallery = document.querySelector(".gallery");
+                
+                const workElement = document.createElement("article");
+                
+                const imageUrlElement = document.createElement("img");
+                imageUrlElement.src = article.imageUrl;
         
-        const sectionGallery = document.querySelector(".gallery");
+                const titleElement = document.createElement("p");
+                titleElement.innerText = article.title;
+                
+                sectionGallery.appendChild(workElement);
+                workElement.appendChild(imageUrlElement);
+                workElement.appendChild(titleElement);
+               
+        }}
+
+        generateProjets(works)
+
         
-        const workElement = document.createElement("article");
-        
-        const imageUrlElement = document.createElement("img");
-        imageUrlElement.src = article.imageUrl;
+     //BOUTON TOUS //
+     const boutonTous = document.querySelector(".btn-tous");
+    
+     boutonTous.addEventListener("click", function () {
+         const afficheTous = works.filter(function (work) {
+             return works;
+             
+         })
+      console.log(afficheTous)
+     
+      document.querySelector(".gallery").innerHTML = "";
+     generateProjets(afficheTous)
+     })
 
-        const titleElement = document.createElement("p");
-        titleElement.innerText = article.title;
-        
-        sectionGallery.appendChild(workElement);
-        workElement.appendChild(imageUrlElement);
-        workElement.appendChild(titleElement);
-}})
-
-// Récupération des object + filtres boutons
 
 
-const boutonObjets = document.querySelector(".btn-objets");
+    
+    // BOUTON OBJETS //
+    const boutonObjets = document.querySelector(".btn-objets");
+    
+    boutonObjets.addEventListener("click", function () {
+        const afficheObjets = works.filter(function (work) {
+            return work.categoryId === 1;
+            
+        })
+     console.log(afficheObjets)
+    
+     document.querySelector(".gallery").innerHTML = "";
+    generateProjets(afficheObjets)
+    })
 
-boutonObjets.addEventListener("click", function () {
-   const listFiltrees = workElement.category(function (category) {
-       return category.id === 1;
-});
-console.log(listFiltrees)
+
+
+    //BOUTON APPARTEMENTS //
+
+    const boutonAppart = document.querySelector(".btn-appart");
+    
+    boutonAppart.addEventListener("click", function () {
+        const afficheAppart = works.filter(function (work) {
+            return work.categoryId === 2;
+            
+        })
+     console.log(afficheAppart)
+    
+     document.querySelector(".gallery").innerHTML = "";
+    generateProjets(afficheAppart)
+    })
+
+
+
+
+    //BOUTON HOTEL & RESTAURANTS //
+
+
+    const boutonHotel = document.querySelector(".btn-hotel");
+    
+    boutonHotel.addEventListener("click", function () {
+        const afficheHotel = works.filter(function (work) {
+            return work.categoryId === 3;
+            
+        })
+     console.log(afficheHotel)
+    
+     document.querySelector(".gallery").innerHTML = "";
+    generateProjets(afficheHotel)
+    })
+
+
 });
