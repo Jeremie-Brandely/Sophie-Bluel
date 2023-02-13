d2381c59da70cbfb18e1d5ab9bd0cbe128176015
Jeremie Brandely <jeremie.brandely@gmail.com>
2023-02-13 10:12:28 +0100

    modif connexion

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -5,35 +5,35 @@
     // ECOUTER LE CLICK DU BOUTON //
 
     const loginBouton = document.querySelector(".login-button");
-    loginBouton.appendChild("#login");
     
-        loginBouton.addEventListener("click", function() {
-        const email = document.getElementById(".email").value;
-        const password = document.getElementById(".password").value;
-    });
+    
+    loginBouton.addEventListener("click", function() {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        
 
 
         fetch("http://localhost:5678/api/users/login", {
             method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({email: ".email", password: ".password"})
+            body: JSON.stringify({email: email, password: password}),
+            headers: { "Content-Type" : "application/json" }
         })
-
-        .then(reponse => reponse.json())
+        .then((reponse) => reponse.json)
         .then((data) => {
             console.log(data);
             
-            if(data.message) {
-                document.querySelector("#error-message").textContent = "Erreur dans l`identifiant ou le mot de passe";
-            }else{
-                localStorage.setItem("access_token", data.token);
+            if(data) {
                 localStorage.setItem("userId", data.userId);
-                window.location.href = "/FrontEnd/";
+                localStorage.setItem("token", data.token);
+                window.location.href = "index.html";
+            }else{
+                
+                document.querySelector("#error-message").textContent = "Erreur dans l`identifiant ou le mot de passe";
             }
         })
         .catch(error => console.error(error))
+    });
     
-    
-    
+
 
 
